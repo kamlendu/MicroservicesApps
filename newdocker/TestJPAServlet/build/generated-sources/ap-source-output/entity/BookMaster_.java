b5a473c7c8d3f0a7ca6f0ec55f533c1b7651d311
kamlendu <kamlendup@gmail.com>
2020-12-02 14:46:04 +0530

    on 02 Nov 2020

---
+++ newdocker/TestJPAServlet/build/generated-sources/ap-source-output/entity/BookMaster_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-12-02T13:51:21")
+@StaticMetamodel(BookMaster.class)
+public class BookMaster_ { 
+
+    public static volatile SingularAttribute<BookMaster, String> authorName;
+    public static volatile SingularAttribute<BookMaster, String> publisherName;
+    public static volatile SingularAttribute<BookMaster, String> synopsis;
+    public static volatile SingularAttribute<BookMaster, String> bookName;
+    public static volatile SingularAttribute<BookMaster, Integer> bookID;
+
+}
